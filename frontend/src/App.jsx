b524821a484b7400bfd73056f26feb04c304694f
Bayazid26 <plabonprodhan731@gmail.com>
2026-04-29 03:20:39 +0600

    fix: correct component imports for Vercel build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./components/Protectedroute";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
